c61b38bd182bc29c71deba1ca7aa3ffc4f9107e3
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-11-15 15:01:23 +0800

    router dom
    config some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div id="screen" className="h-screen overflow-auto bg-stone-900 flex flex-col"></div>
     </>
   );
-}
+};
 
 export default App;
--- frontend/src/app/PageLayout.jsx
+++ frontend/src/app/PageLayout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "../globals.css";
 
 const PageLayout = () => {
   return <div>PageLayout</div>;
 };
 
 export default PageLayout;
+
+
+
+
+function () {
+  return (  );
+}
+
+export default ;
