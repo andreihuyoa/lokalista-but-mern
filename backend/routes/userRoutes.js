4842368cc8aa0948e4dc137a30db337b3cb5ef3e
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-11-19 01:17:27 +0800

    role based auth token validation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import verifyToken from "../middlewares/authMiddleware.js";
+import authorizedRoles from "../middlewares/roleMiddleware.js";
 const router = express.Router();
 
 //Client
-router.get("/client", verifyToken, (req, res) => {
+router.get("/client", verifyToken, authorizedRoles("client"), (req, res) => {
   res.send("Welcome Client");
 });
 
 //Freelancer
-router.get("/freelancer", verifyToken, (req, res) => {
+router.get("/freelancer", verifyToken, authorizedRoles("freelance"), (req, res) => {
   res.send("Welcome Freelancer");
 });
 
