6b4e98ef030fd33dd9664f9a23842b845f811871
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-11-25 17:43:26 +0800

    minor changes(1)
    - utils is needed for shadcn pala
    - removed flex-col

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "../components/ThemeProvider";
 export const Layout = ({ children }) => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="min-h-screen flex flex-col bg-background dark:bg-background pt-2 md:pt-4">
+      <div className="min-h-screen flex bg-background dark:bg-background pt-2 md:pt-4">
         {/* <Header /> */}
         <main className="flex-grow pt-5 md:pt-2">
           <div className="container mx-auto px-2 sm:px-6 lg:px-8">{children}</div>
--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,5 +1,7 @@
+//lib folder are for libraries. implementation of facade pattern
+
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
